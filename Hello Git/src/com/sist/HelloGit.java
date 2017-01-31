82837987b0554def45f99199b2e9ee657c873829
jungousam <jungousam@naver.com>
2017-01-31 14:44:31 +0900

    Create project Create Hello Git

---
+++ Hello Git/src/com/sist/HelloGit.java
@@ -0,0 +1,13 @@
+package com.sist;
+/*
+ * 	Git 환경설정
+ * 	원하는 프로젝트 오른쪽클릭
+ * 	Team -> Shara project
+ * 	Creat 폴더선택 Browse눌러 폴더 지정
+*/
+public class HelloGit {
+	public static void main(String[] args){
+		System.out.println("Hello Git");
+	}
+
+}
